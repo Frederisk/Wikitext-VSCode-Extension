cac000cabf27c43d54c17a1127cc3d81625f1e7c
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2020-02-23 13:00:39 +0800

    Fixed webpack debuger scripts

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,6 @@ Licensed under the MIT License.
 
 //@ts-check
 
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-//@ts-check
-
 'use strict';
 
 const path = require('path');
