fc2f1e1575f5e13116718b8e8c24343882c29e30
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-03-28 18:46:07 +0800

    Update package

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -11,8 +11,9 @@ export function run(): Promise<void> {
     // Create the mocha test
     const mocha = new Mocha({
         ui: 'tdd',
+        color: true
     });
-    mocha.useColors(true);
+    // mocha.useColors(true);
 
     const testsRoot = path.resolve(__dirname, '..');
 
