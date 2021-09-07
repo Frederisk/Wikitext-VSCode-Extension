34ea73a000726272bf23b38585e8742f37975db2
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2021-09-07 18:46:08 +0800

    Simplify getHost method

--- src/export_command/host_function/host.ts
+++ src/export_command/host_function/host.ts
@@ -6,31 +6,30 @@
 import * as vscode from 'vscode';
 
 export async function getHost(): Promise<string | undefined> {
-    async function setHost(): Promise<string | undefined> {
-        const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("wikitext");
-
-        const result: string | undefined = await vscode.window.showInputBox({
+    const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("wikitext");
+    const host: string | undefined = config.get("host");
+    // if host is existed, return it.
+    if (host) { return host; }
+    // else ask to edit
+    const selection = await vscode.window.showWarningMessage(
+        `No Host Be Defined!
+You haven't defined the host of previewer yet, please input host value in the dialog box (or in settings) and try again.`
+        , "Edit", "Cancel");
+    // edit
+    if (selection === 'Edit') {
+        // show the input box
+        const input: string | undefined = await vscode.window.showInputBox({
             prompt: "Please input the host of previewer.",
             placeHolder: "en.wikipedia.org",
             value: config.get("host") || "en.wikipedia.org",
             ignoreFocusOut: false
         });
-        if (result) {
-            config.update("host", result, true);
-            return result;
+        // if input is not null or empty, update setting and return input
+        if (input) {
+            config.update("host", input, true);
+            return input;
         }
     }
-
-    const config: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration("wikitext");
-    const host: string | undefined = config.get("host");
-    if (host) { return host; }
-
-    // Login and Session Management
-    // error, show warnning
-    const result = await vscode.window.showWarningMessage(
-        `No Host Be Defined!
-You haven't defined the host of previewer yet, please input host value in the dialog box (or in settings) and try again.`
-        , "Edit", "Cancel");
-
-    return result === "Edit" ? setHost() : undefined;
+    // or else return undefined
+    return undefined;
 }
